77aa4a7e3cdece144c8021c578914fd412bddf4b
Julian Tambo <julianricardotambo@gmail.com>
2024-02-05 20:32:25 -0500

    primer reto

---
+++ main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    nombre = input("Ingrese sus nombres: ")
+    apellidos = input("Ingrese sus apellidos: ")
+    telefono = input("Ingrese su número de teléfono: ")
+    correo = input("Ingrese su correo electrónico: ")
+
+    print("\n¡Bienvenido, " + nombre + " " + apellidos + "!")
+    print("En breve recibirás un correo a " + correo)
